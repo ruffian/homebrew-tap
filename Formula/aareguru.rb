700dd319c0bf61481eccc3df789bd4f3952aa762
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-27 10:27:39 +0200

    aareguru version v0.3.4

--- Formula/aareguru.rb
+++ Formula/aareguru.rb
@@ -1,9 +1,9 @@
 class Aareguru < Formula
   desc "aare.guru Aaretemperatur & Wetter & Sonne fürs Aareschwimmen - Chills mau wider chli"
   homepage "https://aare.guru/"
-  url "https://github.com/gexclaude/aaregurucli/releases/download/v0.3.3/aareguru_0.3.3_darwin_amd64.tar.gz"
-  version "0.3.3"
-  sha256 "c7738cb48a2a540b64c9dc4788bcf468a4f523906a717751b307ea4cf42786e0"
+  url "https://github.com/gexclaude/aaregurucli/releases/download/v0.3.4/aareguru_0.3.4_darwin_amd64.tar.gz"
+  version "0.3.4"
+  sha256 "a21e73969ceaa89f2dc96787bdd48af2a084a76fd0b587042eb28a15cc484df1"
 
   def install
     bin.install "aareguru"
